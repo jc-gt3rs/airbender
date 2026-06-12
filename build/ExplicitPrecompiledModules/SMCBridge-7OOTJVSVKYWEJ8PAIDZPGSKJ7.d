9313b2dddc852a274d4077ace86518a0e9d7b9ec
jc-gt3rs <antorcris14@gmail.com>
2026-06-12 11:06:30 +0800

    Add standard macOS/Swift .gitignore and remove ignored files from index

--- build/ExplicitPrecompiledModules/SMCBridge-7OOTJVSVKYWEJ8PAIDZPGSKJ7.d
+++
@@ -1,5 +0,0 @@
-moduledependenciestarget: \
-  /Users/johncrisantor/Developer/airbender/com.airbender.helper/SMCClient/module.modulemap \
-  /Users/johncrisantor/Developer/airbender/com.airbender.helper/SMCClient/SMC.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX26.5.sdk/System/Library/Frameworks/IOKit.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/21/include/module.modulemap
